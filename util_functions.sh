a2befef424e15c584101ab5c43336c444fa7663d
sothx <i@sothx.com>
2024-05-24 04:17:48 +0800

    feat: remote

--- util_functions.sh
+++ util_functions.sh
@@ -7,11 +7,11 @@ patch_device_features() {
   # 移除旧版补丁文件
   rm -rf "$MODULE_DEVICE_FEATURES_PATH"
 
-  # 复制私有App权限配置到模块内
+  # 复制系统内配置到模块内
   cp -f "$SYSTEM_DEVICE_FEATURES_PATH" "$MODULE_DEVICE_FEATURES_PATH"
 
-  # 拷贝权限代码片段到模块文件内
   if [[ -f "$MODULE_DEVICE_FEATURES_PATH" ]]; then
+    # 启用息屏听剧/息屏挂机
     sed -i 's/<bool name="remove_screen_off_hold_on">true<\/bool>/<bool name="remove_screen_off_hold_on">false<\/bool>/g' $MODULE_DEVICE_FEATURES_PATH
   fi
 }
